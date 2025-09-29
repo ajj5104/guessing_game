0bcdcc6f6c7bb68e7574df47d11b792467662326
ajj5104 <ajj5104@rit.edu>
2025-09-29 14:41:32 -0400

    Basic "guessing" game (returns input)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;    // input output library
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();  // creates a new mutable string variable called guess
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
